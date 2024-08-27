56016747ad092b88fe6d807a5211562e6197f371
Mohan <mohankandar@1983@gmail.com>
2024-08-27 15:55:59 -0400

    0827_changes

--- vop-framework-autoconfigure/src/main/java/com/wynd/vop/framework/autoconfigure/audit/AuditAutoConfiguration.java
+++ vop-framework-autoconfigure/src/main/java/com/wynd/vop/framework/autoconfigure/audit/AuditAutoConfiguration.java
@@ -6,10 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-/**
- * Created by rthota on 8/24/17.
- */
-
 @Configuration
 @EnableAsync
 public class AuditAutoConfiguration {
